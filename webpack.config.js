7c3bf088a296b57ffa240acdda0ec6340c218a58
stedza93 <stefanmila93@gmail.com>
2018-01-28 10:47:34 +0100

    zavrsen video projekat

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  entry: ["./src/index.js"],
+  output: {
+    path: __dirname,
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devtool: "source-map",
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ["react", "es2015", "stage-1"]
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: "./"
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      sourceMap: true
+    })
+  ]
+};
