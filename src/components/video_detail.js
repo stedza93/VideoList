b5180d32023da68008e02c6c5aa853f3f4e01fa2
stedza93 <stefanmila93@gmail.com>
2018-01-28 12:32:38 +0100

    izmjena nad video detail

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
-    if(!video) {
-        return <div>Loading...</div>;
-    }
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+  console.log(video);
   const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
 
